8afd6db98544cb4220c6a80defecbeb071785000
VidrigeVidar <vidar.angviken@hotmail.com>
2024-02-29 15:43:14 +0100

    fixed Items fetch and mapped it

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,21 +1,20 @@
 import { useState, useEffect } from "react"
 
-
 export default function Items() {
 
   const [items, setItems] = useState([])
 
   useEffect(() => {
-      
+
     async function load() {
       try {
-        const response = await fetch("api/items")
+        const response = await fetch("/api/items")
         const items = await response.json()
         console.log(items)
+        setItems(items)
       } catch (error) {
         console.error("Error message: ", error)
       }
-      setItems(items)
     }
     load()
 
@@ -23,7 +22,13 @@ export default function Items() {
 
   return (
     <>
-    
+      <h1>Games</h1>
+      {
+        items.map(item => <section key={item.id}>
+          <img src={item.img} width="100" />
+          <p>{item.title} | {item.releaseYear} | {item.genre} | Start price: {item.startPrice} | Ending: {item.endDateTime.slice(0, 10)} {item.endDateTime.slice(11, 16)}</p>
+        </section>)
+      }
     </>
   )
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,7 +7,7 @@ export default function Nav() {
       <Link to="/">Home</Link> |&nbsp;
       <Link to="login">Login/Register</Link> |&nbsp;
       <Link to="my-page">Username</Link> |&nbsp; {/*Ska ersätta Login med if-sats*/}
-      <Link to="new-item">Sell</Link> {/*Ska bara synas som inloggad*/}
+      <Link to="new-item">Sell</Link> |&nbsp; {/*Ska bara synas som inloggad*/}
       <Link to="items">Items</Link> {/*Ska bara synas som inloggad*/}
     </nav>
   )
