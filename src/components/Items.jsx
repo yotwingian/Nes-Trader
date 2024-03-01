c30521a89679a77d6052ad2419a6a82bb9880e14
5teef <mustafaquadan@gmail.com>
2024-03-01 02:47:10 +0100

    added a custom renderer to Countdown component!

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -21,13 +21,22 @@ export default function Items() {
 
   }, [])
 
+  const renderer = ({ days, hours, minutes, seconds, completed }) => {
+    if (completed) {
+      return <span>Tiden är ute!</span>
+    } else {
+      return <span>{days} dagar, {hours} timmar, {minutes} minuter och {seconds} sekunder Lägg ditt bud nu!</span>;
+    }
+
+  }
+
   return (
     <>
       <h1>Games</h1>
       {
         items.map(item => <section key={item.id}>
           <img src={item.img} width="100" />
-          <p>{item.title} | {item.releaseYear} | {item.genre} | Start price: {item.startPrice} | Ending: {item.endDateTime.slice(0, 10)} {item.endDateTime.slice(11, 16)} | Auktionen avslutas om: <Countdown date={new Date(item.endDateTime)} /> </p>
+          <p>{item.title} | {item.releaseYear} | {item.genre} | Start price: {item.startPrice} | Auktionen avslutas om: <Countdown date={new Date(item.endDateTime)} renderer={renderer} /> </p>
         </section>)
       }
     </>
