106ef68282cfbc6d93853499134210477ca65a0a
nobotrobot <guybartell@gmail.com>
2024-03-01 10:21:14 +0100

    Changed swedish text to english, updated paths in JSON game file

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -23,9 +23,9 @@ export default function Items() {
 
   const renderer = ({ days, hours, minutes, seconds, completed }) => {
     if (completed) {
-      return <span>Tiden är ute!</span>
+      return <span>Game over!</span>
     } else {
-      return <span>{days} dagar, {hours} timmar, {minutes} minuter och {seconds} sekunder Lägg ditt bud nu!</span>;
+      return <span>{days} days, {hours} hours, {minutes} minutes och {seconds} seconds Bid now!</span>;
     }
 
   }
@@ -36,7 +36,7 @@ export default function Items() {
       {
         items.map(item => <section key={item.id}>
           <img src={item.img} width="100" />
-          <p>{item.title} | {item.releaseYear} | {item.genre} | Start price: {item.startPrice} | Auktionen avslutas om: <Countdown date={new Date(item.endDateTime)} renderer={renderer} /> </p>
+          <p>{item.title} | {item.releaseYear} | {item.genre} | Start price: {item.startPrice} | Game ends in: <Countdown date={new Date(item.endDateTime)} renderer={renderer} /> </p>
         </section>)
       }
     </>
