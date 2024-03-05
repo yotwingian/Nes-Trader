9e7af0ba10c9963856d0a979fab8457d4b868d6a
5teef <mustafaquadan@gmail.com>
2024-03-05 22:25:33 +0100

    Implemented a seperate state for filtering items using useState also added items to the GlobalContext useEffect to load new items when a new item has been added

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,31 +1,36 @@
-import { useContext } from "react"
-import { Link } from "react-router-dom"
-import Countdown from "react-countdown"
-import { GlobalContext } from "./GlobalContext.jsx"
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import Countdown from "react-countdown";
+import { GlobalContext } from "./GlobalContext.jsx";
 
 export default function Items() {
-
-  const { items, filtered, setFiltered } = useContext(GlobalContext)
+  const { items } = useContext(GlobalContext);
+  const [filteredItems, setFilteredItems] = useState(items);
 
   const renderer = ({ days, hours, minutes, seconds, completed }) => {
     if (completed) {
-      return <span>Game over!</span>
+      return <span>Game over!</span>;
     } else {
-      return <span>{days} days, {hours} hours, {minutes} minutes och {seconds} seconds Bid now!</span>;
+      return (
+        <span>
+          {days} days, {hours} hours, {minutes} minutes och {seconds} seconds Bid
+          now!
+        </span>
+      );
     }
-
-  }
+  };
 
   function filter(event) {
-    const searchString = event.target.value.toLowerCase()
+    const searchString = event.target.value.toLowerCase();
 
     const searchResult = items.filter(item =>
-      item.title.toLowerCase().includes(searchString) ||
-      item.releaseYear.toString().includes(searchString) ||
-      item.genre.toLowerCase().includes(searchString) ||
-      item.description.toLowerCase().includes(searchString))
-    
-    setFiltered(searchResult)
+        item.title.toLowerCase().includes(searchString) ||
+        item.releaseYear.toString().includes(searchString) ||
+        item.genre.toLowerCase().includes(searchString) ||
+        item.description.toLowerCase().includes(searchString)
+    );
+
+    setFilteredItems(searchResult);
   }
 
   return (
@@ -35,18 +40,18 @@ export default function Items() {
       </search>
 
       <h1>Games</h1>
-      {
-        filtered.map(item => <section key={item.id}>
-          <Link to={{
-            pathname: `/item-details/${item.id}`,
-          }}>
-
-            <img src={item.img} width="100" />
-            <p>{item.title} | {item.releaseYear} | {item.genre} | Start price: {item.startPrice} | Game ends in: <Countdown date={new Date(item.endDateTime)} renderer={renderer} /> </p>
+      {filteredItems.map((item) => (
+        <section key={item.id}>
+          <Link to={{ pathname: `/item-details/${item.id}` }}>
+            <img src={item.img} width="100" alt={item.title} />
+            <p>
+              {item.title} | {item.releaseYear} | {item.genre} | Start price:{" "}
+              {item.startPrice} | Game ends in:{" "}
+              <Countdown date={new Date(item.endDateTime)} renderer={renderer} />{" "}
+            </p>
           </Link>
-        </section>)
-      }
+        </section>
+      ))}
     </>
-  )
-
+  );
 }
