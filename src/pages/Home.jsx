1ed415d7aebc6927c125c8a6ab8458ce22e49f2e
yotwingian <pawel.bielawski@hotmail.com>
2024-03-11 12:25:57 +0100

    added top 5 latest, ending soon , items to new homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,55 @@
-import { useContext } from "react";
-import { GlobalContext } from "../components/GlobalContext.jsx";
+import { useState, useEffect } from "react";
+import Items from "../components/Items.jsx";
+import { Link } from "react-router-dom"
+import Countdown from "react-countdown"
+import CountdownRenderer from "../components/CountdownRenderer.jsx"
 
 export default function Home() {
-  const contextValue = useContext(GlobalContext);
-  console.log(contextValue);
-  return (
-    <h1>Home</h1>
-  )
+  const [endingSoonItems, setEndingSoonItems] = useState([]);
+  const [latestItems, setLatestItems] = useState([]);
 
+  useEffect(() => {
+    async function load() {
+      const response = await fetch("/api/items");
+      const data = await response.json();
+
+     
+      const sortedByEndingSoon = [...data].sort((a, b) => new Date(a.endDateTime) - new Date(b.endDateTime));
+      setEndingSoonItems(sortedByEndingSoon.slice(0, 5));
+
+      
+      const sortedByLatest = [...data].sort((a, b) => new Date(b.startDateTime) - new Date(a.startDateTime));
+      setLatestItems(sortedByLatest.slice(0, 5));
+    }
+    load();
+  }, []);
+
+  return (
+    <div>
+       { <h1>Top 5 Ending Soon Items</h1> }
+      {endingSoonItems.map(item => (
+        <Link to={{ pathname: `/item-details/${item.id}` }}>
+          <img src={item.img} width="100" alt={item.title} />
+          <p>
+            {item.title} | {item.releaseYear} | {item.genre} | Start price:{" "} {item.startPrice} | Game over in:{" "}
+            <Countdown date={new Date(item.endDateTime)} renderer={CountdownRenderer} />{" "}
+          </p>
+        </Link>
+      ))}
+      
+       { <h1>Top 5 Latest Items</h1> }
+      {latestItems.map(item => (
+        <Link to={{ pathname: `/item-details/${item.id}` }}>
+          <img src={item.img} width="100" alt={item.title} />
+          <p>
+            {item.title} | {item.releaseYear} | {item.genre} | Start price:{" "} {item.startPrice} | Game over in:{" "}
+            <Countdown date={new Date(item.endDateTime)} renderer={CountdownRenderer} />{" "}
+          </p>
+        </Link>
+      ))}
+
+      <h1>All Games</h1>
+      <Items />
+    </div>
+  );
 }
