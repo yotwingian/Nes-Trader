ab0fa53e4cdbde032f8ffadaf7a30d1a3bf9f356
nobotrobot <guybartell@gmail.com>
2024-03-11 15:58:44 +0100

    Created components to return highest bid and total number of bids for individual items

---
+++ src/components/MaxBid.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react"
+
+export default function MaxBid({ id, startPrice }) {
+
+  const [bids, setBids] = useState([])
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch("/api/bids/")
+      const data = await response.json()
+      setBids(data)
+    }
+    load()
+  }, [])
+
+  const thisItemBids = bids.filter(bid => bid.itemId.toString().includes(id))
+  const maxBid = thisItemBids.reduce((acc, bid) => {
+    return (acc = acc > bid.amount ? acc : bid.amount)
+  }, 0)
+
+  if (startPrice < maxBid) {
+    return <>
+      Current bid: {maxBid} kr
+    </>
+  }
+  else {
+    return <>
+      Start price: {startPrice} kr
+    </>
+  }
+
+}
