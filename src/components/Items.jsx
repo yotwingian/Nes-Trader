38404982fb6ee829647b3ecb1afab1019cdb9411
VidrigeVidar <vidar.angviken@hotmail.com>
2024-03-12 09:48:06 +0100

    added MaxBid and TotalBids to Items, removed kr

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 import Countdown from "react-countdown"
 import CountdownRenderer from "./CountdownRenderer.jsx"
+import TotalBids from "../components/TotalBids.jsx"
+import MaxBid from "../components/MaxBid.jsx"
 
 export default function Items() {
 
@@ -74,8 +76,9 @@ export default function Items() {
         <Link to={{ pathname: `/item-details/${item.id}` }}>
           <img src={item.img} width="100" alt={item.title} />
           <p>
-            {item.title} | {item.releaseYear} | {item.genre} | Start price:{" "} {item.startPrice} | Game over in:{" "}
-            <Countdown date={new Date(item.endDateTime)} renderer={CountdownRenderer} />{" "}
+            {item.title} | {item.releaseYear} | {item.genre} |
+            <MaxBid id={parseInt(item.id)} startPrice={parseInt(item.startPrice)} /> | <TotalBids id={parseInt(item.id)} /> | 
+            Game over in:{" "} <Countdown date={new Date(item.endDateTime)} renderer={CountdownRenderer} />{" "}
           </p>
         </Link>
       </section>
