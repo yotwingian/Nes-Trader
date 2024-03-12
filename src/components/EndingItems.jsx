c4dc948c7390b7dec504d4e360b5fae4baf1bb7a
yotwingian <pawel.bielawski@hotmail.com>
2024-03-12 13:08:25 +0100

    seperatad endingitems & latestitems from home

---
+++ src/components/EndingItems.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from "react-router-dom"
+import Countdown from "react-countdown"
+import CountdownRenderer from "../components/CountdownRenderer.jsx"
+import MaxBid from "../components/MaxBid.jsx"
+import TotalBids from "../components/TotalBids.jsx"
+
+const EndingItems = ({ items }) => {
+  return (
+    <div>
+      <h1>Top 5 Ending Soon Items</h1>
+      {items.map(item => (
+        <section key={item.id}>
+          <Link to={{ pathname: `/item-details/${item.id}` }}>
+            <img src={item.img} width="100" alt={item.title} />
+            <p>
+              {item.title} | {item.releaseYear} | {item.genre} |
+              <MaxBid id={parseInt(item.id)} startPrice={parseInt(item.startPrice)} /> | <TotalBids id={parseInt(item.id)} /> |
+              Game over in:{" "} <Countdown date={new Date(item.endDateTime)} renderer={CountdownRenderer} />{" "}
+            </p>
+          </Link>
+        </section>
+      ))}
+    </div>
+  );
+};
+
+export default EndingItems;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,10 @@
 import { useState, useEffect, useContext } from "react";
 import Items from "../components/Items.jsx";
-import { Link } from "react-router-dom"
-import Countdown from "react-countdown"
-import CountdownRenderer from "../components/CountdownRenderer.jsx"
 import MyBids from "../components/MyBids.jsx";
 import MyItems from "../components/MyItems.jsx";
 import { GlobalContext } from "../components/GlobalContext.jsx";
-import TotalBids from "../components/TotalBids.jsx"
-import MaxBid from "../components/MaxBid.jsx"
+import LatestItems from '../components/LatestItems';
+import EndingItems from '../components/EndingItems';
 
 export default function Home() {
   const { isLoggedIn } = useContext(GlobalContext);
@@ -19,11 +16,9 @@ export default function Home() {
       const response = await fetch("/api/items");
       const data = await response.json();
 
-     
       const sortedByEndingSoon = [...data].sort((a, b) => new Date(a.endDateTime) - new Date(b.endDateTime));
       setEndingSoonItems(sortedByEndingSoon.slice(0, 5));
 
-      
       const sortedByLatest = [...data].sort((a, b) => new Date(b.startDateTime) - new Date(a.startDateTime));
       setLatestItems(sortedByLatest.slice(0, 5));
     }
@@ -39,33 +34,8 @@ export default function Home() {
         </>
       )}
 
-       { <h1>Top 5 Ending Soon Items</h1> }
-      {endingSoonItems.map(item => (
-        <section key={item.id}>
-          <Link to={{ pathname: `/item-details/${item.id}` }}>
-            <img src={item.img} width="100" alt={item.title} />
-            <p>
-              {item.title} | {item.releaseYear} | {item.genre} |
-              <MaxBid id={parseInt(item.id)} startPrice={parseInt(item.startPrice)} /> | <TotalBids id={parseInt(item.id)} /> |
-              Game over in:{" "} <Countdown date={new Date(item.endDateTime)} renderer={CountdownRenderer} />{" "}
-            </p>
-          </Link>
-        </section>
-      ))}
-      
-       { <h1>Top 5 Latest Items</h1> }
-      {latestItems.map(item => (
-        <section key={item.id}>
-          <Link to={{ pathname: `/item-details/${item.id}` }}>
-            <img src={item.img} width="100" alt={item.title} />
-            <p>
-              {item.title} | {item.releaseYear} | {item.genre} |
-              <MaxBid id={parseInt(item.id)} startPrice={parseInt(item.startPrice)} /> | <TotalBids id={parseInt(item.id)} /> |
-              Game over in:{" "} <Countdown date={new Date(item.endDateTime)} renderer={CountdownRenderer} />{" "}
-            </p>
-          </Link>
-        </section>
-      ))}
+      <EndingItems items={endingSoonItems} />
+      <LatestItems items={latestItems} />
 
       <h1>All Games</h1>
       <Items />
