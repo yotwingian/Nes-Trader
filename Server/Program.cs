4442a5405866f14eab5adc569c2987ca062b0e77
VidrigeVidar <vidar.angviken@hotmail.com>
2024-04-04 16:21:21 +0200

    Added class Items/method All, gets /items from db
    To replace mock data with data from actual db connection, using MySql.
    Removed mock data from endpoint /items, also tried to match placeholder endpoint
    strings to future methods replacing them, and to make comments clearer.

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,5 +1,4 @@
-// using Server; - Kommer behövas när vi skapar namespace Server; i andra filer.
-
+using Server;
 using MySql.Data.MySqlClient;
 
 MySqlConnection? db = null;
@@ -9,236 +8,15 @@ try
 {
   db = new(connectionString);
   db.Open();
-  
-  var builder = WebApplication.CreateBuilder(args); 
+
+  var builder = WebApplication.CreateBuilder(args);
   builder.Services.AddSingleton(new State(db));
   var app = builder.Build();
-  
-  app.MapGet("/", () => "Home"); // Hämtar ingen data, borde inte behövas?
-  app.MapGet("/items", () => @"
-   [
-      {
-        ""id"": ""1"",
-        ""title"": ""Mario Bros."",
-        ""releaseYear"": 1983,
-        ""genre"": ""Action"",
-        ""description"": ""This is an original copy of the classic Mario Bros. game. The cartridge is in good condition with minor wear and tear from use. The game plays perfectly and brings back the nostalgia of the 80s arcade era. A must-have for any retro game collector!"",
-        ""img"": ""https://i.ebayimg.com/images/g/dt0AAOSwMYZhyMnW/s-l1600.jpg"",
-        ""startDateTime"": ""2024-02-24T10:00:00+01:00"",
-        ""endDateTime"": ""2024-03-10T10:07:00+01:00"",
-        ""startPrice"": ""70"",
-        ""reservePrice"": ""105"",
-        ""bids"": [
-          {
-            ""bidder"": ""test"",
-            ""amount"": 80,
-            ""timestamp"": ""2024-03-07T12:15:00+01:00""
-          },
-          {
-            ""bidder"": ""test2"",
-            ""amount"": 90,
-            ""timestamp"": ""2024-03-07T12:17:00+01:00""
-          }
-        ]
-      },
-      {
-        ""id"": ""2"",
-        ""title"": ""Donkey Kong Junior"",
-        ""releaseYear"": 1983,
-        ""genre"": ""Action"",
-        ""description"": ""This is an original copy of the classic Donkey Kong Junior game. The cartridge is in good condition with minor wear and tear from use. The game plays perfectly and brings back the nostalgia of the 80s arcade era. A must-have for any retro game collector!"",
-        ""img"": ""/img/Donkey_Kong_Junior.jpeg"",
-        ""startDateTime"": ""2024-03-01T10:00:00+01:00"",
-        ""endDateTime"": ""2024-03-24T12:40:00+01:00"",
-        ""startPrice"": ""50"",
-        ""reservePrice"": ""75"",
-        ""bids"": []
-      },
-      {
-        ""id"": ""3"",
-        ""title"": ""Donkey Kong"",
-        ""releaseYear"": 1983,
-        ""genre"": ""Action"",
-        ""description"": ""Original copy of the iconic Donkey Kong game. The cartridge is in excellent condition and the game plays flawlessly. A classic piece for any retro game enthusiast!"",
-        ""img"": ""/img/Donkey_Kong.jpeg"",
-        ""startDateTime"": ""2024-03-08T10:00:00+01:00"",
-        ""endDateTime"": ""2024-03-22T10:00:00+01:00"",
-        ""startPrice"": ""60"",
-        ""reservePrice"": ""90"",
-        ""bids"": []
-      },
-      {
-        ""id"": ""4"",
-        ""title"": ""Popeye"",
-        ""releaseYear"": 1983,
-        ""genre"": ""Action"",
-        ""description"": ""This is an original Popeye game cartridge in good condition. The game is fully functional and offers a nostalgic trip back to the golden age of arcade games. A fun addition to any game collection!"",
-        ""img"": ""/img/Popeye.jpeg"",
-        ""startDateTime"": ""2024-03-15T10:00:00+01:00"",
-        ""endDateTime"": ""2024-03-29T10:00:00+01:00"",
-        ""startPrice"": ""40"",
-        ""reservePrice"": ""60"",
-        ""bids"": []
-      },
-      {
-        ""id"": ""5"",
-        ""title"": ""Donkey Kong Jr. Math"",
-        ""releaseYear"": 1983,
-        ""genre"": ""Educational, Puzzle"",
-        ""description"": ""Rare educational game Donkey Kong Jr. Math. The cartridge is in great condition. Perfect for collectors looking for unique pieces."",
-        ""img"": ""/img/Donkey_Kong_Jr_Math.jpeg"",
-        ""startDateTime"": ""2024-03-22T10:00:00+01:00"",
-        ""endDateTime"": ""2024-04-05T10:00:00+01:00"",
-        ""startPrice"": ""75"",
-        ""reservePrice"": ""113"",
-        ""bids"": []
-      },
-      {
-        ""id"": ""6"",
-        ""title"": ""Baseball"",
-        ""releaseYear"": 1983,
-        ""genre"": ""Sports"",
-        ""description"": ""Original Baseball game for the NES. The cartridge shows some signs of use but is in good working condition. A fun addition to any game collection!"",
-        ""img"": ""/img/Baseball.jpeg"",
-        ""startDateTime"": ""2024-03-29T10:00:00+01:00"",
-        ""endDateTime"": ""2024-04-12T10:00:00+01:00"",
-        ""startPrice"": ""30"",
-        ""reservePrice"": ""45"",
-        ""bids"": []
-      },
-      {
-        ""id"": ""7"",
-        ""title"": ""Donkey Kong Jr. / Jr. Sansuu Lesson"",
-        ""releaseYear"": 1983,
-        ""genre"": ""Compilation"",
-        ""description"": ""This is a compilation of Donkey Kong Jr. games, including the rare Jr. Sansuu Lesson. The cartridge is in good condition and the games work perfectly. A unique find for any retro game collector!"",
-        ""img"": ""/img/Jr_Sansu_Lesson.jpg"",
-        ""startDateTime"": ""2024-04-05T10:00:00+01:00"",
-        ""endDateTime"": ""2024-04-19T10:00:00+01:00"",
-        ""startPrice"": ""80"",
-        ""reservePrice"": ""120"",
-        ""bids"": []
-      },
-      {
-        ""id"": ""8"",
-        ""title"": ""Gomoku Narabe Renju"",
-        ""releaseYear"": 1983,
-        ""genre"": ""Strategy / tactics"",
-        ""description"": ""Gomoku Narabe Renju is a strategy game for the NES. This original copy is in good condition and is a unique find for any retro game collector."",
-        ""img"": ""/img/Gomoku_Narabe_Renju.jpg"",
-        ""startDateTime"": ""2024-04-12T10:00:00+01:00"",
-        ""endDateTime"": ""2024-04-26T10:00:00+01:00"",
-        ""startPrice"": ""55"",
-        ""reservePrice"": ""83"",
-        ""bids"": []
-      },
-      {
-        ""id"": ""9"",
-        ""title"": ""Mahjong"",
-        ""releaseYear"": 1983,
-        ""genre"": ""Strategy"",
-        ""description"": ""This is an original Mahjong game for the NES. The cartridge is in excellent condition. A great addition to any game collection!"",
-        ""img"": ""/img/Mahjong.jpeg"",
-        ""startDateTime"": ""2024-04-19T10:00:00+01:00"",
-        ""endDateTime"": ""2024-05-03T10:00:00+01:00"",
-        ""startPrice"": ""45"",
-        ""reservePrice"": ""68"",
-        ""bids"": []
-      },
-      {
-        ""id"": ""10"",
-        ""title"": ""Popeye no Eigo Asobi"",
-        ""releaseYear"": 1983,
-        ""genre"": ""Action, Educational, Puzzle"",
-        ""description"": ""Popeye no Eigo Asobi is a unique educational game featuring the beloved character Popeye. The cartridge is in good condition and the game plays well. A fun and educational addition to any game collection!"",
-        ""img"": ""/img/Popeye_no_Eigo_Asobi.jpeg"",
-        ""startDateTime"": ""2024-04-26T10:00:00+01:00"",
-        ""endDateTime"": ""2024-05-10T10:00:00+01:00"",
-        ""startPrice"": ""50"",
-        ""reservePrice"": ""75"",
-        ""bids"": []
-      },
-      {
-        ""id"": ""11"",
-        ""title"": ""Excitebike"",
-        ""releaseYear"": 1984,
-        ""genre"": ""Sports, Racing / Driving"",
-        ""description"": ""Original Excitebike game for the NES. The cartridge is in good condition with minor wear. A classic racing game that's fun for all ages!"",
-        ""img"": ""/img/Excitebike.jpeg"",
-        ""startDateTime"": ""2024-05-03T10:00:00+01:00"",
-        ""endDateTime"": ""2024-05-17T10:00:00+01:00"",
-        ""startPrice"": ""35"",
-        ""reservePrice"": ""53"",
-        ""bids"": []
-      },
-      {
-        ""id"": ""12"",
-        ""title"": ""Mappy"",
-        ""releaseYear"": 1984,
-        ""genre"": ""Action"",
-        ""description"": ""This is an original Mappy game cartridge in good condition. The game is fully functional and offers a nostalgic trip back to the golden age of arcade games. A fun addition to any game collection!"",
-        ""img"": ""/img/Mappy.jpeg"",
-        ""startDateTime"": ""2024-05-10T10:00:00+01:00"",
-        ""endDateTime"": ""2024-05-24T10:00:00+01:00"",
-        ""startPrice"": ""40"",
-        ""reservePrice"": ""60"",
-        ""bids"": []
-      },
-      {
-        ""id"": ""13"",
-        ""title"": ""Pac-Man"",
-        ""releaseYear"": 1984,
-        ""genre"": ""Action"",
-        ""description"": ""Original copy of the iconic Pac-Man game. The cartridge is in excellent condition and the game plays flawlessly. A classic piece for any retro game enthusiast!"",
-        ""img"": ""/img/Pac-Man.jpeg"",
-        ""startDateTime"": ""2024-05-17T10:00:00+01:00"",
-        ""endDateTime"": ""2024-05-31T10:00:00+01:00"",
-        ""startPrice"": ""60"",
-        ""reservePrice"": ""90"",
-        ""bids"": []
-      },
-      {
-        ""id"": ""14"",
-        ""title"": ""Galaxian"",
-        ""releaseYear"": 1984,
-        ""genre"": ""Action"",
-        ""description"": ""This is an original Galaxian game cartridge in good condition. The game is fully functional and offers a nostalgic trip back to the golden age of arcade games. A fun addition to any game collection!"",
-        ""img"": ""/img/Galaxian.jpeg"",
-        ""startDateTime"": ""2024-05-24T10:00:00+01:00"",
-        ""endDateTime"": ""2024-06-07T10:00:00+01:00"",
-        ""startPrice"": ""40"",
-        ""reservePrice"": ""60"",
-        ""bids"": []
-      },
-      {
-        ""id"": ""15"",
-        ""title"": ""Nuts & Milk"",
-        ""releaseYear"": 1984,
-        ""genre"": ""Action"",
-        ""description"": ""Rare game Nuts & Milk for the NES. The cartridge is in great condition. Perfect for collectors looking for unique pieces."",
-        ""img"": ""/img/Nuts&Milk.jpeg"",
-        ""startDateTime"": ""2024-06-01T10:00:00+01:00"",
-        ""endDateTime"": ""2024-06-15T10:00:00+01:00"",
-        ""startPrice"": ""75"",
-        ""reservePrice"": ""112"",
-        ""bids"": []
-      },
-      {
-        ""id"": ""2e16"",
-        ""title"": ""Teenage Mutant Ninja Turtles"",
-        ""releaseYear"": ""1989"",
-        ""genre"": ""Action"",
-        ""description"": ""A game"",
-        ""img"": ""https://www.arkad.nu/9917/uploads/fbf34639-01f8-4e96-a45a-490d05d78f2a.jpg"",
-        ""startDateTime"": ""2024-03-14T09:14"",
-        ""endDateTime"": ""2024-03-21T09:14"",
-        ""startPrice"": ""10"",
-        ""reservePrice"": ""70""
-      }
-    ]
-  "); // Mockdata, ska ersättas med db
-  app.MapGet("/items/ending-soon", () => "EndingItems");
-  app.MapGet("/items/latest", () => "LatestItems");
+
+  app.MapGet("/", () => "NES Trader Server");
+  app.MapGet("/items", Items.All);
+  app.MapGet("/items/ending-soon", () => "Items.EndingSoon");
+  app.MapGet("/items/latest", () => "Items.Latest");
   app.MapGet("/mylistings", () => @"
   [
       {
@@ -281,8 +59,9 @@ try
         ""bids"": []
       }
   ]
-  "); // Mockdata, ska tas bort! Ersätts med "/items/{user}"
-  app.MapGet("/items/{user}", () => "MyItems");
+  ");
+  // ^ Mockdata, ska tas bort! Ersätts med "/items/{user}" v
+  app.MapGet("/items/{user}", () => "Items.User");
   app.MapGet("/bids", () => @"
   [
       {
@@ -426,7 +205,8 @@ try
         ""itemId"": 2
       }
     ]
-  "); // Mockdata, ska tas bort?
+  ");
+  // ^ Mockdata, ska tas bort! Behövs en endpoint för alla bids?
   app.MapGet("/mybids", () => @"
   [
       {
@@ -469,8 +249,9 @@ try
         ""bids"": []
       }
   ]
-  "); // Mockdata, ska tas bort! Ersätts med "/bids/{user}"
-  app.MapGet("/bids/{user}", () => "MyBids");
+  ");
+  // ^ Mockdata, ska tas bort! Ersätts med "/bids/{user}" v
+  app.MapGet("/bids/{user}", () => "Bids.User");
   app.MapGet("/users", () => @"
   [
       {
@@ -481,16 +262,16 @@ try
         ""id"": ""2357""
       }
   ]
-  "); // Mockdata, ska tas bort?
-  app.MapPost("/login", () => "Login");
-  app.MapPost("/register", () => "Register");
-  app.MapGet("/item-details/{slug}", () => "ItemDetails"); // Döpa om till "/item"?
-  app.MapGet("/item-details/{slug}/bids", () => "ItemBids");
-  app.MapPost("/item-details/{slug}/new-bid", () => "NewBid");
-  app.MapPost("/new-item", () => "NewItem");
+  ");
+  // ^ Mockdata, ska tas bort! Behövs en endpoint för alla users?
+  app.MapPost("/users/login", () => "Users.Login");
+  app.MapPost("/users/register", () => "Users.Register");
+  app.MapGet("/item-details/{slug}", () => "Item.Details"); // Döpa om till "/item"?
+  app.MapGet("/item-details/{slug}/bids", () => "Item.Bids");
+  app.MapPost("/item-details/{slug}/new-bid", () => "Item.NewBid");
+  app.MapPost("/new-item", () => "NewItem.Add");
 
   app.Run("http://localhost:3000");
-
 }
 catch (MySqlException e)
 {
@@ -501,7 +282,4 @@ finally
   db?.Close();
 }
 
-record State(MySqlConnection Db);
-
-
-
+public record State(MySqlConnection DB);
