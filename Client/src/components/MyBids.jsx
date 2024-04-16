f9a7a0de379366a645eee59d5de31c30278f6693
5teef <mustafaquadan@gmail.com>
2024-04-16 21:45:23 +0200

    Tweaks
    Tweaking some react codes with if else, changed some css for
    items-container.

--- Client/src/components/MyBids.jsx
+++ Client/src/components/MyBids.jsx
@@ -17,31 +17,33 @@ export default function MyBids() {
         const response = await fetch("/api/bids/" + user)
         const data = await response.json()
         setUserBids(data)
-         }
+      }
       load()
     }
   }, [isLoggedIn])
 
-  return (
-    
+
+  return <>
+    {userBids != "" ? (
       <div>
-      <h1>Player Bids</h1>
-      <div className="items-container">
-        {userBids.map(item => (
-          <section key={item.slug}>
-            <div >
-              <Link to={{ pathname: `/item/${item.slug}` }} style={{ textDecoration: 'none' }}>
-                <img src={item.img} alt={item.title} />
-                <h5>{item.title}</h5>
-                <div><p className="itemstext">{item.releaseYear} | {item.genre}</p></div>
-                <div className="items"><MaxBid slug={item.slug} startPrice={item.startPrice} /> | <TotalBids slug={item.slug} /></div>
-                <div className="items">{" "} <Countdown date={new Date(item.endDateTime)} renderer={CountdownRenderer} />{" "}</div>
-              </Link>
-            </div>
-          </section>
-        ))}
+        <h1 id="mybids">Player Bids</h1>
+        <div className="items-container">
+          {userBids.map(item => (
+            <section key={item.slug}>
+              <div >
+                <Link to={{ pathname: `/item/${item.slug}` }} style={{ textDecoration: 'none' }}>
+                  <img src={item.img} alt={item.title} />
+                  <h5>{item.title}</h5>
+                  <div><p className="itemstext">{item.releaseYear} | {item.genre}</p></div>
+                  <div className="items"><MaxBid slug={item.slug} startPrice={item.startPrice} /> | <TotalBids slug={item.slug} /></div>
+                  <div className="items">{" "} <Countdown date={new Date(item.endDateTime)} renderer={CountdownRenderer} />{" "}</div>
+                </Link>
+              </div>
+            </section>
+          ))}
         </div>
       </div>
-    
-  )
+    ) : (null)}
+  </>
+  
 }
--- Client/src/pages/MyPage.jsx
+++ Client/src/pages/MyPage.jsx
@@ -1,12 +1,20 @@
+import { useContext } from "react"
+import { GlobalContext } from "../components/GlobalContext.jsx"
 import MyBids from "../components/MyBids.jsx"
 import MyItems from "../components/MyItems.jsx"
+
 export default function MyPage() {
+  const { isLoggedIn } = useContext(GlobalContext)
 
   return <>
-    <h1>Player Page</h1>
-    
-    <MyBids />
-    <MyItems />
+    {isLoggedIn && (
+      <div>
+        <h1 id="mypage">Player Page</h1>
+        <MyBids />
+        <MyItems />
+      </div>
+    )}
+    <h1>Please sign in!</h1>
   </>
-
+   
 }
