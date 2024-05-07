ed785b0965c422202fe38e318a77c79afb6dab45
nobotrobot <guybartell@gmail.com>
2024-05-07 14:38:08 +0200

    Created tests for sorting by year and title
    So that we can test when you sort by year or title, the items are sorted alphanumerically ascending by year or title.

--- cypress/specs/games.feature
+++ cypress/specs/games.feature
@@ -9,3 +9,13 @@ Feature: Games
     | searchTerm |
     | Mario |
     | Donkey Kong |
+
+Scenario: Sorting items by Title
+  Given I am on the "/games" page
+  When I select #Title
+  Then the first letter of the first title should be the same or earlier alphabetically than the first letter of the second title
+
+  Scenario: Sorting items by Release Year
+    Given I am on the "/games" page
+    When I select #Release Year
+    Then the year of the first item should be the same or earlier than the year of the second item
--- cypress/specs/step_definitions/games.js
+++ cypress/specs/step_definitions/games.js
@@ -12,3 +12,43 @@ Then('I should see only items with titles containing {string}', (searchTerm) => 
   });
 });
 
+When('I select #Title', () => {
+  cy.get('select').select('Title')
+});
+
+Then('the first letter of the first title should be the same or earlier alphabetically than the first letter of the second title', () => {
+  cy.get(':nth-child(1) > .itemsCard > a > h5').invoke('text').then(firstString => {
+
+    
+    cy.get(':nth-child(2) > .itemsCard > a > h5').invoke('text').then(secondString => {
+
+      const firstLetter = firstString.charAt(0).toLowerCase(); 
+      const secondLetter = secondString.charAt(0).toLowerCase();
+
+      assert(firstLetter <= secondLetter, 'the first letter of the first title should be the same or earlier alphabetically than the first letter of the second title');
+
+    });
+  });
+});
+
+When('I select #Release Year', () => {
+  cy.get('select').select('Release Year')
+});
+
+Then('the year of the first item should be the same or earlier than the year of the second item', () => {
+  cy.get(':nth-child(1) > .itemsCard > a > .itemsFlex > :nth-child(1) > .itemstext').invoke('text').then(firstString => {
+
+
+    cy.get(':nth-child(2) > .itemsCard > a > .itemsFlex > :nth-child(1) > .itemstext').invoke('text').then(secondString => {
+
+      const firstYear = firstString.slice(0, 4);
+      const secondYear = secondString.slice(0, 4);
+      
+      const firstNumber = parseFloat(firstYear);
+      const secondNumber = parseFloat(secondYear);
+
+      assert(firstNumber <= secondNumber, 'the year of the first item should be the same or earlier than the year of the second item');
+
+    });
+  });
+});
