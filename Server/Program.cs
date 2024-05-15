2e7b14facebe6f784dec32871ef1d1fd44c9c4cc
VidrigeVidar <vidar.angviken@hotmail.com>
2024-05-15 13:46:36 +0200

    Added logic for serving dist folder

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,13 +1,38 @@
 using System.Security.Claims;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
 using Server;
 
-State state = new("server=localhost;uid=root;pwd=docker;database=nes_trader;port=3306");
+State state = new("server=localhost;uid=root;pwd=mypassword;database=nes_trader;port=3306");
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+  serverOptions.ListenAnyIP(3000);
+});
+
 builder.Services.AddAuthentication().AddCookie("nes-trader.user");
 builder.Services.AddAuthorizationBuilder().AddPolicy("user", policy => policy.RequireClaim(ClaimTypes.NameIdentifier));
 builder.Services.AddSingleton(state);
 var app = builder.Build();
+
+var distPath = Path.Combine(app.Environment.ContentRootPath, "../Client/dist");
+var fileProvider = new PhysicalFileProvider(distPath);
+
+app.UseHttpsRedirection();
+
+app.UseDefaultFiles(new DefaultFilesOptions
+{
+  FileProvider = fileProvider,
+  DefaultFileNames = new List<string> { "index.html" }
+});
+
+app.UseStaticFiles(new StaticFileOptions
+{
+  FileProvider = fileProvider,
+  RequestPath = ""
+});
 
 app.MapGet("/", () => "NES Trader Server");
 
@@ -30,6 +55,18 @@ app.MapPost("/api/users/login", Users.Login);
 app.MapPost("/api/users/register", Users.Register);
 app.MapDelete("/api/users/delete/{user}", Users.Delete);
 
-app.Run("http://localhost:3000");
+app.UseRouting();
+
+app.MapFallback(async context =>
+{
+  string path = context.Request.Path.Value;
+  if (!path.StartsWith("/api"))
+  {
+    context.Response.ContentType = "text/html";
+    await context.Response.SendFileAsync(Path.Combine(distPath, "index.html"));
+  }
+});
+
+app.Run();
 
 public record State(string DB);
